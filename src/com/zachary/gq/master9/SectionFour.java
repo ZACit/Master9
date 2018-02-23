41f232f4a4556b727aa7366f8b2c9e3cb21695c1
Zac <hiremytech@gmail.com>
2018-02-23 08:49:30 -0700

    Initial commit

---
+++ src/com/zachary/gq/master9/SectionFour.java
@@ -0,0 +1,23 @@
+package com.zachary.gq.master9;
+
+public class SectionFour {
+
+    public static void main(String[] args) {
+
+        System.out.println("123");
+
+        // Java 9 Master Class |  "What's New in Java 9" Section 4, Lecture 19
+
+        // Create a new class called "Processhandle", name it, and asign..
+
+        ProcessHandle currProcess = ProcessHandle.current();
+        System.out.println("PID: " + currProcess.pid());
+
+
+        // .Info is an interface? @2:25
+        ProcessHandle.Info currProcessInfo = currProcess.info();
+        System.out.println("Info: " + currProcessInfo);
+
+
+    }
+}
